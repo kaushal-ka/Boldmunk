3a4aacf4abf56deed3e0b4ffa6b6a07a7016410c
kaushal-ka <kaushalkarnakayansh@gmail.com>
2025-11-27 00:29:49 +0545

    Updated accounts

---
+++ accounts/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from .models import Account
+class RegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Enter Password',
+        'class': 'form-control',
+    }))
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Confirm Password',
+        'class': 'form-control',
+    }))
+    class Meta:
+        model   = Account
+        fields  = ['first_name', 'last_name', 'phone_number', 'email', 'password']
+
+    def __init__(self, *args, **kwargs):
+        super(RegistrationForm, self).__init__(*args, **kwargs)
+        for field in
