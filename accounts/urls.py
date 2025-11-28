0587ed99fd91f845098c4c4966ca7a3f7d0a2ebc
kaushal karna <kaushalkarnakayansh@gmail.com>
2025-11-28 07:14:08 +0545

    Updating to fill raw dashboard.html in our code from another pc

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,8 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+
     # email verification path
     path('activate<uidb64>/<token>/', views.activate, name='activate'),
 
